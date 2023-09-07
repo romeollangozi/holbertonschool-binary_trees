011ea1461684741934803768328ce09282d1d529
romeollangozi <romeollangozi123@gmail.com>
2023-09-07 01:27:38 -0700

    retest

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,10 +10,12 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *node = NULL;
+
 	if (parent == NULL)
 		return (NULL);
 
-	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+	node = malloc(sizeof(binary_tree_t));
 	if (node == NULL)
 		return (NULL);
 

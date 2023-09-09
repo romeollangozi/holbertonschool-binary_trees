cb25c68e44ec064bf7fd1da5453d9cf29882bd2f
romeollangozi <romeollangozi123@gmail.com>
2023-09-09 06:21:01 -0700

    function that finds the numberr of leaves in binary tree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -11,14 +11,16 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
+	else if (tree->left == NULL && tree->right == NULL)
 	{
 		return (1);
 	}
-
+	else
+	{
 		size_t leaves;
 
 		leaves = binary_tree_leaves(tree->left);
 		leaves += binary_tree_leaves(tree->right);
 		return (leaves);
+	}
 }

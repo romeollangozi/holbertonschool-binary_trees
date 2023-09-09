3db80b9c77feab690e3d28f5659c7bccaaa965f7
romeollangozi <romeollangozi123@gmail.com>
2023-09-09 05:30:45 -0700

    function that find the depth of a binary tree

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,7 +9,9 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (tree == NULL || (tree->parent == NULL))
 	return (0);
-
-	size_t depth = binary_tree_depth(tree->parent);
-	return (depth + 1);
+	else
+	{
+		size_t depth = binary_tree_depth(tree->parent);
+		return (depth + 1);
+	}
 }
